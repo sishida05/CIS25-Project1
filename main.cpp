62661e2a4d8c3d8a09ce2f228165bfa8a41381ce
sishida05 <30080170@cc.peralta.edu>
2026-02-18 19:43:47 +0000

    Setup project file structure

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "markov.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    
+
+    return 0;
+}
---
+++ markov.cpp
@@ -0,0 +1,8 @@
+#include "marov.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
