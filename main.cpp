2659fff1e72b434cdcc92ebfb3a7cdfe139322a0
sishida05 <30080170@cc.peralta.edu>
2026-02-18 19:49:42 +0000

    Implemented readWordsFromFile

--- main.cpp
+++ main.cpp
@@ -8,6 +8,18 @@ int main() {
     cout << joinWords(testWords, 0, 2) << endl;  // Should print: the cat
     cout << joinWords(testWords, 1, 3) << endl;  // Should print: cat sat down
 
+    ofstream testFile("test.txt");
+    testFile.open();
+    testFile << "This is a test file that must contain words!" << endl;
+    testFile << "This is another line of words that are in this file!" << endl;
+
+    string words[1000];
+    int count = readWordsFromFile("test.txt", words, 1000);
+    cout << "Read " << count << " words" << endl;
+    for (int i = 0; i < 10 && i < count; i++) {
+    cout << words[i] << endl;
+    }
+
 
     return 0;
 }
